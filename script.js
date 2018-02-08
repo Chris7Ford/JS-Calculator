726ef141dfa3e656aa11629700cbfdac87d89dc8
Chris7Ford <cford2132@gmail.com>
2018-02-07 20:28:39 -0600

    First commit
    Needs more styling
    Would like to make title bigger
    will reconsider fonts and colors
    maybe combine separate displays

---
+++ script.js
@@ -0,0 +1,9 @@
+/*to do list for calculater:
+1.  Make sure you cannot enter more than one decimal per number
+2.  Make sure you cannot put more than 1 arithmetic button in a rom
+3.  
+
+
+colors:
+http://paletton.com/#uid=35m0x0kaVz84jP27qHbeJtFiHpX
+*/
