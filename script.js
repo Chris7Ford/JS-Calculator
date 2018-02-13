9cd58006589428e5d9306e960aa14713696238b6
Chris7Ford <cford2132@gmail.com>
2018-02-12 21:29:45 -0600

    Fix event listener for on click
    made some global variables local
    this seemed to fix problems
    made a few color changes
    event listener pushes an array
    array is converted to string
    next, add to display in HTML

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 2.  Make sure you cannot put more than 1 arithmetic button in a rom
 3.  
 
-
+---
+Making separate functions for each action to be able to return multiple values
 colors:
 http://paletton.com/#uid=35m0x0kaVz84jP27qHbeJtFiHpX
 */
 
-window.onload=function(){
+document.addEventListener('DOMContentLoaded', function () {
 
 var ce = document.querySelector(".ce");
 var c = document.querySelector(".c");
@@ -26,44 +27,51 @@ var one = document.querySelector(".one");
 var two = document.querySelector(".two");
 var three = document.querySelector(".three");
 var add = document.querySelector(".add");
-var neg_pos = document.querySelector(".toggle_neg_pos");
+var neg_pos = document.querySelector(".neg_pos");
 var zero = document.querySelector(".zero");
 var decimal= document.querySelector(".decimal");
 var equals= document.querySelector(".equals");
 
-var buttonId=0;
-var mainDisplay=[];
-var runningDisplay=[];
+runningDisplayView=document.querySelector(".runningTotal");
 
-function pushRunning(){
-    runningDisplay.push(buttonId.innerHTML);
-    console.log("hello");
+runningDisplayString=0;
+buttonId=0;
+mainDisplay=[];
+runningDisplayArray=[];
+
+function pushRunningArray(){
+    runningDisplayArray.push(buttonId.innerHTML);
+    runningDisplayString=runningDisplayArray.join('');
+    return runningDisplayString;
 }
 
+function activity() {
+    pushRunningArray();
+}
 
-ce.addEventListener("click",function(){buttonId=ce; pushRunning();})
-c.addEventListener("click",function(){buttonId=c; pushRunning();})
+ce.addEventListener("click",function(){buttonId=ce; activity();})
+c.addEventListener("click",function(){buttonId=c; activity();})
 if(del) {
-del.addEventListener("click",function(){buttonId=del; pushRunning();})
+del.addEventListener("click",function(){buttonId=del; activity();})
 }
-divide.addEventListener("click",function(){buttonId=divide; pushRunning();return cellId;})
-seven.addEventListener("click",function(){buttonId=seven; pushRunning();return cellId;})
-eight.addEventListener("click",function(){buttonId=eight; pushRunning();return cellId;})
-nine.addEventListener("click",function(){buttonId=nine; pushRunning();return cellId;})
-multiply.addEventListener("click",function(){buttonId=multiply; pushRunning();return cellId;})
-four.addEventListener("click",function(){buttonId=four; pushRunning();return cellId;})
-five.addEventListener("click",function(){buttonId=five; pushRunning();return cellId;})
-six.addEventListener("click",function(){buttonId=six; pushRunning();return cellId;})
-subtract.addEventListener("click",function(){buttonId=subtract; pushRunning();return cellId;})
-one.addEventListener("click",function(){buttonId=one; pushRunning();return cellId;})
-two.addEventListener("click",function(){buttonId=two; pushRunning();return cellId;})
-three.addEventListener("click",function(){buttonId=three; pushRunning();return cellId;})
-add.addEventListener("click",function(){buttonId=add; pushRunning();return cellId;})
-neg_pos.addEventListener("click",function(){buttonId=neg_pos; pushRunning();return cellId;})
-zero.addEventListener("click",function(){buttonId=zero; pushRunning();return cellId;})
-decimal.addEventListener("click",function(){buttonId=decimal; pushRunning();return cellId;})
-equals.addEventListener("click",function(){buttonId=equals; pushRunning();return cellId;})
+divide.addEventListener("click",function(){buttonId=divide; activity();return buttonId;})
+seven.addEventListener("click",function(){buttonId=seven; activity();return buttonId;})
+eight.addEventListener("click",function(){buttonId=eight; activity();return buttonId;})
+nine.addEventListener("click",function(){buttonId=nine; activity();return buttonId;})
+multiply.addEventListener("click",function(){buttonId=multiply; activity();return buttonId;})
+four.addEventListener("click",function(){buttonId=four; activity();return buttonId;})
+five.addEventListener("click",function(){buttonId=five; activity();return buttonId;})
+six.addEventListener("click",function(){buttonId=six; activity();return buttonId;})
+subtract.addEventListener("click",function(){buttonId=subtract; activity();return buttonId;})
+one.addEventListener("click",function(){buttonId=one; activity();return buttonId;})
+two.addEventListener("click",function(){buttonId=two; activity();return buttonId;})
+three.addEventListener("click",function(){buttonId=three; activity();return buttonId;})
+add.addEventListener("click",function(){buttonId=add; activity();return buttonId;})
+neg_pos.addEventListener("click",function(){buttonId=neg_pos; activity();return buttonId;})
+zero.addEventListener("click",function(){buttonId=zero; activity();return buttonId;})
+decimal.addEventListener("click",function(){buttonId=decimal; activity();return buttonId;})
+equals.addEventListener("click",function(){buttonId=equals; activity();return buttonId;})
 
 
 
-}
+})
